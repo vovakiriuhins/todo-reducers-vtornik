04558b8ac77eb5ee2651cab43d1de7a9599315d6
vovakiriuhins <vova11999977@gmail.com>
2023-05-31 23:45:15 +0300

    action creator, useDispatch

---
+++ src/reducers/filterReducer.tsx
@@ -0,0 +1,21 @@
+import {FilterValuesType} from "../App";
+
+export const filterReducer = (state: FilterValuesType, action: FilterReducerACType) => {
+  switch (action.type) {
+      case 'FILTER-TASKS': {
+          // return state
+      }
+      default: return state
+  }
+}
+
+type FilterReducerACType = ReturnType<typeof filterReducerAC>
+
+export const filterReducerAC = (value: FilterValuesType) => {
+    return {
+        type: 'FILTER-TASKS',
+        payload: {
+            value
+        }
+    } as const
+}
---
+++ src/reducers/tasksReducer.tsx
@@ -0,0 +1,43 @@
+import { v1 } from "uuid";
+import { TaskType } from "../Todolist";
+
+export const tasksReducer = (state: TaskType[], action: TsarType) => {
+    switch (action.type) {
+        case 'REMOVE-TASK': {
+            // let filteredTasks = tasks.filter(t => t.id !== id);
+            // setTasks(filteredTasks);
+            return state.filter(t=>t.id !== action.payload.id)
+        }
+        case 'ADD-TASK': {
+            let newTasks = { id: v1(), title: action.payload.title, isDone: false };
+            // let newTasks = [task, ...tasks];
+            // setTasks(newTasks);
+            return [newTasks,...state]
+        }
+        default: return state;
+    }
+};
+
+type TsarType = RemoveTaskACType | AddTaskACType
+
+type RemoveTaskACType = ReturnType<typeof removeTaskAC>
+
+export const removeTaskAC = (id: string) => {
+    return {
+        type: 'REMOVE-TASK',
+        payload: {
+            id
+        }
+    } as const
+}
+
+type AddTaskACType = ReturnType<typeof addTaskAC>
+
+export const addTaskAC = (title: string) => {
+    return {
+        type: 'ADD-TASK',
+        payload: {
+            title
+        }
+    }as const
+}
